bde5697f9ee40ce859e9deab063d9ef5d58588d9
yarkyawsoe <yarkyawsoe@zandotech.com>
2019-07-26 11:03:09 +0630

    add controller, sensor and transaction added

--- htc/models/sensor_transaction.py
+++ htc/models/sensor_transaction.py
@@ -5,7 +5,7 @@ from odoo import api, fields, models
 
 class SensorTransaction(models.Model):
     _name = 'htc.sensor_transaction'
-    
+
     site_id = fields.Many2one("htc.site", string="Site", required=True)
     sensor_id = fields.Many2one("htc.sensor", string="Sensor", required=True)
     transaction_date = fields.Datetime("Transaction Date", required=True)
@@ -16,3 +16,5 @@ class SensorTransaction(models.Model):
     week = fields.Integer("Week", required=True)
     day = fields.Integer("Day", required=True)
     method = fields.Char("Method")
+    start_time = fields.Char("Start Time")
+    end_time = fields.Char("End Time")
