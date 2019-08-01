14c852a24757527e4ec4a37b455a100dd7b23815
Aung Myo <aungmyoswe@ZANDO.local>
2019-08-01 09:22:37 +0630

    Update Model

--- htc/models/noti_email.py
+++ htc/models/noti_email.py
@@ -7,7 +7,7 @@ class NotificationEmail(models.Model):
     _inherit = "res.partner"
 
     @api.multi
-    def email_notify(self, record, total, types, limit_count):
+    def email_notify(self, record,total,types,limit_count):
         # domain = [('name', 'ilike', 'HTC Mail_For_Count_2')]
         # domain_server = [('name', 'ilike', 'notification')]
         # template = self.env['mail.template'].search(domain, limit=1)
@@ -37,9 +37,9 @@ class NotificationEmail(models.Model):
         # else:
         #     return False
         domain = [('name', 'ilike', 'HTC Notify')]
-        to_emails = []
+        to_emails=[]
         process_count = record.process_count
-        device_id = record.sensor_id.device_name
+        device_id = record.sensor_id.device_id
         user_name = partner = None
         # .['sensor'].device_id
         template = self.env['mail.template'].search(domain, limit=1)
@@ -55,38 +55,31 @@ class NotificationEmail(models.Model):
             # template.email_to="yarkyawsoe@zandotech.com"
             template.email_from = temp.email_to = email = users = None
             template.subject = "Notify HTC for NN" + str(device_id)
-            email = self.env['ir.mail_server'].search([("active", "=", True)])
+            email = self.env['ir.mail_server'].search([])
             users = self.env['res.users'].search([])
             if email:
                 template.email_from = email.smtp_user
             if users:
                 for user in users:
-                    if user.enable_notify_count is True:
+                    if user.enable_notify_count == True:
                         if not user_name:
                             user_name = user.partner_id.display_name
                         if not partner:
                             partner = user.partner_id.id
                             template.partner_to = partner
                         to_emails.append(user.partner_id.email)
-            # template.emial_to = to_emails[0]
-            template.emial_to = 'aungmyoswe@zandotech.com'
-            # to_emails.pop()
+            template.emial_to=to_emails[0]
+            to_emails.pop()
             # template.email_cc = ','.join(to_emails)
             template.device_id = device_id
             template.count_type = types
             template.process_count = process_count
             template.process_limit_count = limit_count
-            template.body_html = str(
-                'Dear' + str(user_name) +
-                ',<br/><br/>We provide notification for ' + str(device_id) +
-                '.<br/>Total count : ' + str(total) +
-                '<br/>Informed Count : ' + str(process_count) +
-                '<br/>Always inform after next ' + str(limit_count) + '.<br/>')
+            template.body_html =str('Dear'+str(user_name)+',<br/><br/>We provide notification for '+str(device_id)+'.<br/>Total count : '+str(total)+'<br/>Informed Count : '+ str(process_count)+'<br/>Always inform after next '+str(limit_count)+'.<br/>')
             template.send_mail(temp1, True)
             return True
         else:
             return False
-
 
 class MailTemplate(models.Model):
     _inherit = "mail.template"
@@ -97,14 +90,10 @@ class MailTemplate(models.Model):
     process_count = fields.Integer("Inform Count", store=False)
     process_limit_count = fields.Integer("Limit Count", store=False)
 
-
 class Users(models.Model):
     _inherit = "res.users"
 
-    enable_notify_count = fields.Boolean("Enable Notify Count?",
-                                         default=False,
-                                         store=True)
-
+    enable_notify_count = fields.Boolean("Enable Notify Count?", default=False, store=True)
 
 class Message(models.Model):
     _inherit = 'mail.message'
@@ -113,11 +102,10 @@ class Message(models.Model):
     def _get_record_name(self, values):
         """ Return the related document name, using name_get. It is done using
             SUPERUSER_ID, to be sure to have the record name correctly stored. """
-        print("values", values)
+        print ("values", values)
         model = values.get('model', self.env.context.get('default_model'))
         res_id = values.get('res_id', self.env.context.get('default_res_id'))
         if not model or not res_id or model not in self.env:
             return False
-        print('MAIL MSG',
-              self.env[model].sudo().browse(res_id).name_get()[0][1])
+        print ('MAIL MSG', self.env[model].sudo().browse(res_id).name_get()[0][1])
         return self.env[model].sudo().browse(res_id).name_get()[0][1]
--- htc/models/site_group_import.py
+++ htc/models/site_group_import.py
@@ -1,12 +1,12 @@
-from odoo import models, fields, api,_
+from odoo import models, fields, api, _
 from datetime import datetime
 from io import StringIO
 import base64
 import xlrd
 from xlrd import open_workbook
-from odoo.exceptions import UserError,ValidationError
+from odoo.exceptions import UserError, ValidationError
 
-header_fields = ['Mac Address','XML Format']
+header_fields = ['Mac Address', 'XML Format']
 
 header_indexes = {}
 
@@ -17,40 +17,46 @@ class ImportSiteGroup(models.Model):
 
     import_fname = fields.Char('Filename', size=128)
     import_file = fields.Binary('File', required=True)
-    
+
     err_log = ''
 
     def _check_file_ext(self):
         for import_file in self:
-            if '.dat' or '.DAT' or '.xls' or '.xlsx' in import_file.import_fname:return True
-            else: return False
+            if '.dat' or '.DAT' or '.xls' or '.xlsx' in import_file.import_fname:
+                return True
+            else:
+                return False
         return True
-    
-    _constraints = [(_check_file_ext, "Please import EXCEL file!", ['import_fname'])]
-    
+
+    _constraints = [(_check_file_ext, "Please import EXCEL file!",
+                     ['import_fname'])]
+
     # ## Load excel data file
     def get_excel_datas(self, sheets):
         result = []
         for s in sheets:
-            cell_row=0
+            cell_row = 0
             for row in range(cell_row, s.nrows):
                 values = []
                 for col in range(0, s.ncols):
                     values.append(s.cell(row, col).value)
                 result.append(values)
         return result
-    
+
     # ## Check excel row headers with header_fields and define header indexes for database fields
     def get_headers(self, line):
-        print ("line=>",line[0].strip())
+        print("line=>", line[0].strip())
         if line[0].strip() not in header_fields:
 
-            raise ValidationError(_('Error :'), _("Error while processing the header line %s.\\n\nPlease check your Excel separator as well as the column header fields") % line)
+            raise ValidationError(
+                _('Error :'),
+                _("Error while processing the header line %s.\\n\nPlease check your Excel separator as well as the column header fields"
+                  ) % line)
         else:
             # ## set header_fields to header_index with value -1
             for header in header_fields:
-                header_indexes[header] = -1  
-                     
+                header_indexes[header] = -1
+
             col_count = 0
             for ind in range(len(line)):
                 if line[ind] == '':
@@ -59,20 +65,22 @@ class ImportSiteGroup(models.Model):
                 elif ind == len(line) - 1:
                     col_count = ind + 1
                     break
-            
-            for i in range(col_count):                
+
+            for i in range(col_count):
                 header = line[i].strip().lower()
                 if header not in header_fields:
-                    self.err_log += '\n' + _("Invalid Excel File, Header Field '%s' is not supported !") % header
+                    self.err_log += '\n' + _(
+                        "Invalid Excel File, Header Field '%s' is not supported !"
+                    ) % header
                 else:
                     header_indexes[header] = i
-                                
-            for header in header_fields:
-                if header_indexes[header] < 0:                    
-                    self.err_log += '\n' + _("Invalid Excel file, Header '%s' is missing !") % header
- 
 
-    ######### Read data and import to database ##########       
+            for header in header_fields:
+                if header_indexes[header] < 0:
+                    self.err_log += '\n' + _(
+                        "Invalid Excel file, Header '%s' is missing !") % header
+
+    ######### Read data and import to database ##########
     def import_data(self):
         val = {}
         header_line = True
@@ -86,20 +94,21 @@ class ImportSiteGroup(models.Model):
         val['site_group_name'] = active.site_group_name
         val['site_group_code'] = active.site_group_code
         val['license_code'] = active.license_code
-        site_group_id = site_group_obj.search([('site_group_code','=',val['site_group_code'])])
+        site_group_id = site_group_obj.search([('site_group_code', '=',
+                                                val['site_group_code'])])
         if not site_group_id:
             site_group_id = site_group_obj.create(val).id
-        print ("site_groupw1=>",site_group_id)
-        import_file = self.import_file 
+        print("site_groupw1=>", site_group_id)
+        import_file = self.import_file
         file_name = self.import_fname
-        print ("file name",file_name)
+        print("file name", file_name)
         r = []
-        if file_name.find('.xls') !=-1 or file_name.find('.xlsx') !=-1:
+        if file_name.find('.xls') != -1 or file_name.find('.xlsx') != -1:
             lines = base64.decodestring(import_file)
-            print ("lines",lines)
+            print("lines", lines)
             wb = open_workbook(file_contents=lines)
-            r = self.get_excel_datas(wb.sheets())    
-        all_data=[]
+            r = self.get_excel_datas(wb.sheets())
+        all_data = []
         created_counnt = 0
         updated_count = 0
         skipped_count = 0
@@ -111,32 +120,32 @@ class ImportSiteGroup(models.Model):
                 self.get_headers(ln)
                 header_line = False
             else:
-                if ln and ln[0] and ln[0][0] not in ['#','']:
-                    import_vals={}
+                if ln and ln[0] and ln[0][0] not in ['#', '']:
+                    import_vals = {}
                     import_vals['Mac Address'] = ln[0]
                     import_vals['XML Format'] = ln[1]
-                    all_data.append(import_vals)       
-                    print ("DATA",all_data)
+                    all_data.append(import_vals)
+                    print("DATA", all_data)
             if all_data:
                 for data in all_data:
-                    print ('------------')
+                    print('------------')
                     excel_row = all_data.index(data) + 2
-                    print ('excel row => ' + str(excel_row))
-                    print ('data ' + str(data))
-                    print (data)
-                    sen_val ={}
+                    print('excel row => ' + str(excel_row))
+                    print('data ' + str(data))
+                    print(data)
+                    sen_val = {}
                     mac_address = data['Mac Address']
                     xml_format = data['XML Format']
                     if mac_address and xml_format:
-                        sensor_ids = sensor_obj.search([('mac_address','ilike',mac_address)])
+                        sensor_ids = sensor_obj.search([
+                            ('mac_address', 'ilike', mac_address)
+                        ])
                         sen_val = {
-                        'mac_address':mac_address,
-                        'xml_format': xml_format,
-                        'site_group_id':site_group_id.id,
+                            'mac_address': mac_address,
+                            'xml_format': xml_format,
+                            'site_group_id': site_group_id.id,
                         }
-                        print ("sensor=>",sensor_ids)
                         if not sensor_ids:
                             sensor_obj.create(sen_val)
                         else:
                             sensor_ids.write(sen_val)
-              
